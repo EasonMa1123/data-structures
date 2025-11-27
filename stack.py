3947f28a91d155be543175c6c518c8b1005b54b0
Eason Ma <163453353+EasonMa1123@users.noreply.github.com>
2025-11-27 10:02:45 +0000

    Implement stack sorting and helper methods
    Added sorting functionality and helper methods to the stack class.

--- stack.py
+++ stack.py
@@ -36,5 +36,52 @@ class Stack:
     # check if stack is full
     def isFull(self):
         return self.top == self.capacity - 1
+    
+    def printStack(self):
+        return self.arr
+    
+    def sort(self):
+        if self.pop == -1:
+            return 
+        temp_item = self.pop()
+        tempStack1 = Stack(self.capacity)
+        tempStack2 = Stack(self.capacity)
+        compare_Item = self.pop()
+        while not(self.isSorted):
+            self.split(temp_item,compare_Item,tempStack1,tempStack2)
+            self.merge(temp_item,tempStack1,tempStack2)
+        
+  
+        
+    def isSortedhelper(self,n):
+        arr = self.arr
+        # Base case
+        if (n == 0 or n == 1):
+            return True
+            
+        # Check if current and previous elements are in order
+        # and recursively check the rest of the array
+        return (arr[n - 1] >= arr[n - 2] and self.isSortedhelper(arr, n - 1))
+                
+    def isSorted(self):
+        
+        n = len(self.arr)
+        
+        return self.isSortedhelper( n)
 
+    def split(self,temp_item,compare_Item,stack1,stack2):
+        while True:
+            if temp_item >= compare_Item:
+                stack1.push(compare_Item)
+            elif temp_item < compare_Item:
+                stack2.push(compare_Item)
+            compare_Item = self.pop()
+            if compare_Item == -1:
+                return
 
+    def merge(self,temp_item,stack1,stack2):
+        while stack1.isEmpty() == False:
+            self.push(stack1.pop())
+        self.push(temp_item)
+        while stack2.isEmpty() == False:
+            self.push(stack2.pop())
