f8e38b81807eedb6a729d68ec28e9bab362688a6
Eason Ma <163453353+EasonMa1123@users.noreply.github.com>
2025-12-08 12:31:14 +0000

    Implement node, edge, and graph classes
    Added classes for node, edge, and graph structures.

--- graph.py
+++ graph.py
@@ -1,1 +1,27 @@
+class node:
+    def __init__(self,value):
+        self.__value = value
 
+    def getValue(self):
+        return self.__value
+
+class edge:
+    def __init__(self):
+        self.__nodeHead = None
+        self.__nodeTail = None
+        self.__weight = 0
+    
+    def connect(self,node1,node2,weight):
+        self.__nodeHead = node1
+        self.__nodeTail = node2
+
+    def setWeight(self,weight):
+
+        self.__weight = weight
+
+class graph:
+    def __init__(self):
+        pass
+
+    def addNode(self,value):
+        new_node = node(value)
